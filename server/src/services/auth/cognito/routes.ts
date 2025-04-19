b5b076b387738892a9bef71821c5940cd151a41a
msho <mayo_s@hotmail.co.uk>
2025-04-19 21:16:24 +0100

    auth using cookies

--- server/src/services/auth/cognito/routes.ts
+++ server/src/services/auth/cognito/routes.ts
@@ -1,17 +1,35 @@
-import client from 'openid-client';
+import { loginAction } from './actions/login_action';
+import { returnCallbackAction } from './actions/return_callback_action';
 
-export const loginRoute = () => {
-  const code_verifier = client.randomPKCECodeVerifier();
-  const code_challenge = await client.calculatePKCECodeChallenge(code_verifier);
-  const state = client.randomState();
-  let parameters = {
-    redirect_uri: process.env.COGNITO_CALLBACK_URL,
-    code_challenge,
-    code_challenge_method: 'S256',
-    state
-  }
-  const congnitoLoginURL = client.buildAuthorizationUrl(config, parameters).href;
+export const loginRoute = async (req: any, res: any) => {
+  const { state, code_verifier, congnitoLoginURL } = await loginAction();
+
   res.cookie('state', state, { httpOnly: true, signed: true });
   res.cookie('code_verifier', code_verifier, { httpOnly: true, signed: true });
   res.send(JSON.stringify({ congnitoLoginURL }));
-}
+};
+
+export const callbackRoute = async (req: any, res: any) => {
+  try {
+    const tokens = await returnCallbackAction(req);
+    res.cookie('ACCESS_TOKEN', tokens.access_token, { httpOnly: true, signed: true });
+    res.cookie('REFRESH_TOKEN', tokens.refresh_token, { httpOnly: true, signed: true });
+    res.cookie('ID_TOKEN', tokens.id_token);
+    res.clearCookie('state');
+    res.clearCookie('code_verifier');
+    res.send(tokens);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+};
+
+export const logoutRoute = async (req: any, res: any) => {
+  req.session.destroy();
+  const logoutUrl = `https://${process.env.COGNITO_USER_POOL_ID}.auth.${process.env.AWS_REGION}.amazoncognito.com/logout?client_id=${process.env.COGNITO_CLIENT_ID}&logout_uri=${process.env.COGNITO_LOGOUT_URL}`;
+  res.redirect(logoutUrl);
+  res.clearCookie('ACCESS_TOKEN');
+  res.clearCookie('REFRESH_TOKEN');
+  res.clearCookie('ID_TOKEN');
+  res.send('Logged out');
+};
--- server/src/services/server/express/server.ts
+++ server/src/services/server/express/server.ts
@@ -1,25 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import session from 'express-session';
-import { dynamoDBSessionStorage } from 'src/services/database/dynamodb/session_storage';
-
-type UserInfo = {
-  id: string;
-  email: string;
-};
-
-declare module 'express-session' {
-  interface SessionData {
-    userInfo: UserInfo;
-  }
-}
+// import session from 'express-session';
+import cookieParser from 'cookie-parser';
+// import { dynamoDBSessionStorage } from 'src/services/database/dynamodb/session_storage';
 
 export const server = (() => {
   const app = express();
   const port = process.env.PORT || 5000;
 
   initCors(app);
-  initSession(app);
+  // initSession(app);
+  app.use(cookieParser(process.env.COOKIE_SECRET));
 
   const startServer = () => {
     app.listen(port, () => {
@@ -47,21 +38,21 @@ function initCors(app: express.Application) {
   app.use(cors(corsConfig));
 }
 
-function initSession(app: express.Application) {
-  const sessionConfig = {
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    // signed: !!process.env.____,
-    store: dynamoDBSessionStorage(),
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict' as 'strict',
-      // maxAge: 3600000,
-      maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in milliseconds
-    },
-  };
+// function initSession(app: express.Application) {
+//   const sessionConfig = {
+//     secret: process.env.SESSION_SECRET as string,
+//     resave: false,
+//     saveUninitialized: false,
+//     // signed: !!process.env.____,
+//     store: dynamoDBSessionStorage(),
+//     cookie: {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === 'production',
+//       sameSite: 'strict' as 'strict',
+//       // maxAge: 3600000,
+//       maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in milliseconds
+//     },
+//   };
 
-  app.use(session(sessionConfig));
-}
+//   app.use(session(sessionConfig));
+// }
